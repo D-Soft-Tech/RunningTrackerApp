71413852365f0d593c54e62284812e58aa60b62d
DSoftTech <81322397+DSoftTech@users.noreply.github.com>
2021-09-25 08:38:01 +0100

    Added Maps api key and map view to TrackinFragment Got it up and running

--- app/src/main/java/com/example/runningtrackerapp/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/example/runningtrackerapp/ui/fragments/TrackingFragment.kt
@@ -6,21 +6,78 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import com.example.runningtrackerapp.R
+import com.example.runningtrackerapp.databinding.FragmentTrackingBinding
 import com.example.runningtrackerapp.ui.viewModel.MainViewModel
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class TrackingFragment : Fragment() {
 
     private val mainViewModel: MainViewModel by viewModels()
+    private var _binding: FragmentTrackingBinding? = null
+    private val binding get() = _binding!!
+    private var mapObject: GoogleMap? = null
+    private lateinit var mapView: MapView
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tracking, container, false)
+        _binding = FragmentTrackingBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mapView = binding.mapView
+
+        // Because we aren't using the map fragment, 
+        // hence we need to manage the lifecycle of the mapView by ourself
+        mapView.onCreate(savedInstanceState)
+
+        mapView.getMapAsync {
+            mapObject = it
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    // Managing other lifecycles of the mapView inside the corresponding lifecycles of the host fragment
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
     }
 }
